a9c15d6cb8062ba86f8c5df92a6aa4ce90d0d249
yosefbeder <dryosefbeder@gmail.com>
2021-09-06 09:13:11 +0200

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from '../styles/404-page.module.scss';
+
+const NotFound = () => {
+  return (
+    <main className={classes.container}>
+      <h1>404: Page Not Found!</h1>
+    </main>
+  );
+};
+
+export default NotFound;
